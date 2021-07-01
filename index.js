51a02bcca1f89fa514feb89e25b3308220195f29
Mari Taksa <welch.mari@gmail.com>
2021-07-01 16:11:30 -0500

    commit tree f02cb8890260ec888338f49e8d7647e6180dcb4c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const result = drivers.filter(driver => driver.substring(0,name.length).toLowerCase() === name.substring(0,name.length).toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(drivers, name) {
+    const result = drivers.filter(driver => driver.substring(0, 1).toLowerCase() === name.substring(0, 1).toLowerCase())
+    return result;
+}
+
+function matchName(drivers, name) {
+    const result = drivers.filter(driver => driver.name === name)
+    return result;
+}
